c76adc87c583c13574bd49d1fe96340f4d297b6d
MinahilIrfan98 <iminahil30@gmail.com>
2024-04-08 15:46:29 -0700

    Your descriptive commit message here

---
+++ assignment2/abc.js
@@ -0,0 +1,22 @@
+let a= 10
+let b=20
+let c=30
+let sum= a+b+c
+console.log(sum)
+let sub=c-b
+console.log(sub)
+let multiply=a*b*c
+console.log(multiply)
+let divide= b/a
+console.log(divide)
+
+let module=c%b
+console.log(module)
+console.log(a)
+let displayCalulations = document.getElementById("displayCalulations")
+displayCalulations.innerHTML = `<p>${a}</p>`
+totalsum.innerHTML =`<p>${sum}</p>`
+totalsub.innerHTML = `<p>${sub}</p>`
+totalmultiply.innerHTML =`<p>${multiply}</p>`
+totaldivide.innerHTML = `<p>${divide}</p>`
+modulus.innerHTML =`<p>${module}</p>`
